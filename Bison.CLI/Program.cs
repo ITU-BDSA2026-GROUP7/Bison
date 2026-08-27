878a0c38a7f8324929393aea92ae271c186a2fe2
Abdallah Alhamedi <aalh@itu.dk>
2026-08-27 14:52:45 +0200

    implemented reading from csv file

--- Bison.CLI/Program.cs
+++ Bison.CLI/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+using System.IO;
+String line;
+
+StreamReader sr = new StreamReader("C:\\Users\\AB\\Downloads\\bison_observe_cli_db.csv");
+line = sr.ReadLine();
+while (line != null)
+{
+    Console.WriteLine(line);
+    line = sr.ReadLine();
+}
+sr.Close();
+Console.ReadLine();
