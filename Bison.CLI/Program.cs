af140154b4c1fc3bc7b8fc11236ca0f42df15ae9
BareStorm <stoh@itu.dk>
2026-08-27 17:01:17 +0200

    Add observe command for storing observations

--- Bison.CLI/Program.cs
+++ Bison.CLI/Program.cs
@@ -1,17 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 using System.IO;
-String line;
 
-StreamReader sr = new StreamReader("bison_observe_cli_db.csv");
-sr.ReadLine();
-line = sr.ReadLine();
-while (line != null)
-{   
-    String[] values = line.Split(",");
-    DateTimeOffset dateTime = DateTimeOffset.FromUnixTimeSeconds(long.Parse(values[2]));
-    String output = values[0] + " @ " + dateTime.ToString("MM/dd/yy HH:mm:ss") + ": " + values[1];
-    Console.WriteLine(output);
-    line = sr.ReadLine();
+String filename = "bison_observe_cli_db.csv";
+
+if (args.Length > 0 && args[0] == "observe")
+{
+    String observation = args[1];
+    String author = Environment.UserName;
+    long timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+    File.AppendAllText(
+        filename,
+        $"{author},{observation},{timestamp}\n"
+    );    
 }
-sr.Close();
+else
+{
+    StreamReader sr = new StreamReader(filename);
+    sr.ReadLine();
+
+    String? line = sr.ReadLine();
+
+    while (line != null)
+    {   
+        String[] values = line.Split(",");
+
+        DateTimeOffset dateTime = 
+            DateTimeOffset.FromUnixTimeSeconds(long.Parse(values[2]));
+        
+        String output = 
+            values[0] + " @ " + 
+            dateTime.ToString("MM/dd/yy HH':'mm':'ss") + 
+            ": " + values[1];
+
+        Console.WriteLine(output);
+
+        line = sr.ReadLine();
+    }
+
+
+    sr.Close();
+}
