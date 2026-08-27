d4bb2781f539da06e46e576f91ce6bf962bff8f8
Abdallah Alhamedi <aalh@itu.dk>
2026-08-27 16:17:37 +0200

    implemented unix time conversion

--- Bison.CLI/Program.cs
+++ Bison.CLI/Program.cs
@@ -4,10 +4,14 @@ using System.IO;
 String line;
 
 StreamReader sr = new StreamReader("bison_observe_cli_db.csv");
+sr.ReadLine();
 line = sr.ReadLine();
 while (line != null)
-{
-    Console.WriteLine(line);
+{   
+    String[] values = line.Split(",");
+    DateTimeOffset dateTime = DateTimeOffset.FromUnixTimeSeconds(long.Parse(values[2]));
+    String output = values[0] + " @ " + dateTime.ToString("MM/dd/yy HH:mm:ss") + ": " + values[1];
+    Console.WriteLine(output);
     line = sr.ReadLine();
 }
 sr.Close();
