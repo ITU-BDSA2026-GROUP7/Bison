caa0bc8194149fa9bec341f041deac7c6b76cc1d
Eskild Kristiansen <eskk@itu.dk>
2026-09-03 16:13:25 +0200

    Refactor persistence into SimpleDB library
    Extract CSV read/write logic into a new SimpleDB class library,
    implementing IDatabaseRepository<T> via CSVDatabase<T>. Program.cs
    now depends on this abstraction instead of touching CSV directly.
    Also add .gitignore to stop tracking build artifacts and IDE files.

--- Bison.CLI/Program.cs
+++ Bison.CLI/Program.cs
@@ -1,68 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-using System.IO;
-using CsvHelper;
-using CsvHelper.Configuration;
-using System.Globalization;
+using SimpleDB;
 
-String filename = "bison_observe_cli_db.csv";
+const string filename = "bison_observe_cli_db.csv";
 
-// Writing
+IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>(filename);
+
 if (args.Length > 0 && args[0] == "observe")
 {
-    String observation = args[1];
-    String author = Environment.UserName;
+    string observation = args[1];
+    string author = Environment.UserName;
     long timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
 
-    // Creates the obejct we're storing
-    Cheep cheep = new Cheep(author, observation, timestamp);
-
-    // Opens the CSV file without deleting what's already there
-    using (StreamWriter sw = new StreamWriter(filename, append: true))
-    using (CsvWriter csv = new CsvWriter(sw, CultureInfo.InvariantCulture))
-    {
-        // CsvHelper takes Cheep object and turns it into valid CSV
-        csv.WriteRecord(cheep);
-
-        // Moves to the next CSV row, giving us the newline we previously manually added
-        csv.NextRecord();
-    }
+    database.Store(new Cheep(author, observation, timestamp));
 }
-
-// Reading
 else
 {
-    using (StreamReader sr = new StreamReader(filename))
-    using (CsvReader csv = new CsvReader(sr, CultureInfo.InvariantCulture))
+    foreach (Cheep cheep in database.Read())
     {
-        csv.Context.RegisterClassMap<CheepMap>();
+        DateTimeOffset dateTime = DateTimeOffset.FromUnixTimeSeconds(cheep.Timestamp);
 
-        IEnumerable<Cheep> cheeps = csv.GetRecords<Cheep>();
+        string output =
+            cheep.Author + " @ " +
+            dateTime.ToString("MM/dd/yy HH':'mm':'ss") +
+            ": " + cheep.Message;
 
-        foreach (Cheep cheep in cheeps)
-        {
-            DateTimeOffset dateTime =
-                DateTimeOffset.FromUnixTimeSeconds(cheep.Timestamp);
-
-            String output =
-                cheep.Author + " @ " +
-                dateTime.ToString("MM/dd/yy HH':'mm':'ss") +
-                ": " + cheep.Message;
-
-            Console.WriteLine(output);
-        }
+        Console.WriteLine(output);
     }
 }
 
-// Cheep record type which CsvHelper can map CSV fields into
 public record Cheep(string Author, string Message, long Timestamp);
-
-public sealed class CheepMap : ClassMap<Cheep>
-{
-    public CheepMap()
-    {
-        Parameter("Author").Name("Author");
-        Parameter("Message").Name("Observation");
-        Parameter("Timestamp").Name("Timestamp");
-    }
-}
