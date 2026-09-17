53638ded00af384dc99ae52cd317b95fa7f9c1a5
Abdallah Alhamedi <aalh@itu.dk>
2026-09-18 00:00:06 +0200

    jaja

--- src/Bison.CLI/Models.cs
+++
@@ -1,4 +0,0 @@
-namespace Bison.CLI;
-public record ObservationRequest(string Author, string Message, long Timestamp, string? Location = null);
-
-public record CommentRequest(string Author, string Message, long Timestamp, int ObservationId, string? Location = null);
