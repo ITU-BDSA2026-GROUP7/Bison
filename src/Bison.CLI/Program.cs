f9636b85ac98f397b63ccc0d3fddc9cafaf87018
Frederik Nordentoft <fnor@itu.dk>
2026-09-23 15:23:27 +0200

    feat(cli): add propose and proposals commands

--- src/Bison.CLI/Program.cs
+++ src/Bison.CLI/Program.cs
@@ -2,6 +2,7 @@ using System.Net.Http.Json;
 using System.CommandLine;
 using System.CommandLine.Parsing;
 using static UserInterface;
+using SimpleDB;
 
 string baseUrl =
     Environment.GetEnvironmentVariable("BISON_SERVER_URL")
@@ -10,9 +11,12 @@ string baseUrl =
 var client = new HttpClient();
 client.BaseAddress = new Uri(baseUrl);
 
+var taxonomy = TaxonomyLoader.Load();
+
 var locationArgument = new Argument<string>("location");
 var messageArgument = new Argument<string>("message");
 var idArgument = new Argument<int>("id");
+var taxonArgument = new Argument<string>("taxon");
 
 var observeCommand = new Command("observe", "add a new observation");
 observeCommand.Arguments.Add(messageArgument);
@@ -27,6 +31,13 @@ commentCommand.Arguments.Add(messageArgument);
 var discussionCommand = new Command("discussion", "show all comments for an observation");
 discussionCommand.Arguments.Add(idArgument);
 
+var proposeCommand = new Command("propose", "propose a taxon for an observation");
+proposeCommand.Arguments.Add(idArgument);
+proposeCommand.Arguments.Add(taxonArgument);
+
+var proposalsCommand = new Command("proposals", "show all proposed taxons for an observation");
+proposalsCommand.Arguments.Add(idArgument);
+
 var locationCommand = new Command("location", "show all observations for a location");
 locationCommand.Arguments.Add(locationArgument);
 
@@ -35,6 +46,8 @@ rootCommand.Subcommands.Add(observeCommand);
 rootCommand.Subcommands.Add(readCommand);
 rootCommand.Subcommands.Add(commentCommand);
 rootCommand.Subcommands.Add(discussionCommand);
+rootCommand.Subcommands.Add(proposeCommand);
+rootCommand.Subcommands.Add(proposalsCommand);
 rootCommand.Subcommands.Add(locationCommand);
 
 observeCommand.SetAction(async ParseResult =>
@@ -83,6 +96,45 @@ discussionCommand.SetAction(async ParseResult =>
 
     PrintComments(comments!);
     
+    return 0;
+});
+
+proposeCommand.SetAction(async ParseResult =>
+{
+    int observationId = ParseResult.GetValue(idArgument);
+    string input = ParseResult.GetValue(taxonArgument)!;
+    string author = Environment.UserName;
+    long timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+    string? taxonId = taxonomy.GetById(input)?.TaxonId
+        ?? taxonomy.GetByVernacularName(input)?.TaxonId;
+
+    if (taxonId is null)
+    {
+        Console.WriteLine($"Unknown taxon: {input}");
+        return 1;
+    }
+
+    var proposal = new ProposalRequest(author, taxonId, timestamp, observationId);
+    var response = await client.PostAsJsonAsync("/proposal", proposal);
+
+    if (!response.IsSuccessStatusCode)
+    {
+        Console.WriteLine(await response.Content.ReadAsStringAsync());
+        return 1;
+    }
+
+    return 0;
+});
+
+proposalsCommand.SetAction(async ParseResult =>
+{
+    int observationId = ParseResult.GetValue(idArgument);
+    var proposals = await client.GetFromJsonAsync<List<ProposalRequest>>(
+        $"/proposals?observationId={observationId}");
+
+    PrintProposals(proposals!, taxonomy);
+
     return 0;
 });
 
